5d643a2009da80888b3b8332125c73333b93c7d9
Dauren <dauren280601@gmail.com>
2022-05-02 03:00:59 +0600

    Added

---
+++ app/src/main/java/com/example/project/fragments/ChangePasswordFragment.kt
@@ -0,0 +1,66 @@
+package com.example.project.fragments
+
+import android.os.Bundle
+import android.text.TextUtils
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import androidx.navigation.fragment.findNavController
+import com.example.project.DBKeys
+import com.example.project.R
+import com.example.project.databinding.FragmentChangePasswordBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class ChangePasswordFragment : Fragment() {
+    private var _binding: FragmentChangePasswordBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var database: DatabaseReference
+
+    private var password = ""
+    private var password2 = ""
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentChangePasswordBinding.inflate(inflater, container, false)
+        database = Firebase.database.getReference(DBKeys.USERS)
+        binding.btnChangePassword.setOnClickListener {
+            checkPassword()
+        }
+        return binding.root
+    }
+
+    private fun checkPassword(){
+        password = binding.password.text.toString().trim()
+        password2 = binding.password2.text.toString().trim()
+        when {
+            TextUtils.isEmpty(password) -> {
+                binding.password.error = "Please enter a password"
+            }
+            password.length < 6 -> {
+                binding.password.error = "Password must at least 6 characters long"
+            }
+            TextUtils.isEmpty(password2) -> {
+                binding.password.error = "Please repeat a password"
+            }
+            password != password2 -> {
+                binding.password2.error = "Passwords not equal"
+            }
+            else -> {
+                confirmNewPassword()
+            }
+        }
+    }
+
+    private fun confirmNewPassword(){
+        database.child(DBKeys.user!!).child("password").setValue(password)
+        binding.btnChangePassword.onEditorAction(EditorInfo.IME_ACTION_DONE)
+        findNavController().navigate(R.id.action_changePasswordFragment_to_menuFragment)
+    }
+}
--- app/src/main/java/com/example/project/fragments/EnterPasswordFragment.kt
+++ app/src/main/java/com/example/project/fragments/EnterPasswordFragment.kt
@@ -5,19 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.Toast
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.example.project.DBKeys
 import com.example.project.R
 import com.example.project.databinding.FragmentEnterPasswordBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class EnterPasswordFragment : Fragment() {
     private var _binding: FragmentEnterPasswordBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var database: DatabaseReference
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentEnterPasswordBinding.inflate(inflater, container, false)
+        database = Firebase.database.getReference(DBKeys.USERS)
+        binding.btnPassword.setOnClickListener {
+            val password = binding.txtPassword.text.toString()
+            database.child(DBKeys.user!!).get().addOnSuccessListener {
+                if (it.child("password").value.toString() == password) {
+                    binding.btnPassword.onEditorAction(EditorInfo.IME_ACTION_DONE)
+                    findNavController().navigate(R.id.action_enterPasswordFragment_to_changePasswordFragment)
+                }
+                else{
+                    Toast.makeText(activity, "Incorrect password", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
         return binding.root
     }
-
 }
