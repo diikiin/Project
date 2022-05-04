f57f1a9f38803d1876daff6c866f58b05b36e1bc
Dauren <dauren280601@gmail.com>
2022-05-05 03:46:55 +0600

    Added

--- app/src/main/java/com/example/project/fragments/PaymentFragment.kt
+++ app/src/main/java/com/example/project/fragments/PaymentFragment.kt
@@ -1,5 +1,6 @@
 package com.example.project.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.TextUtils
 import androidx.fragment.app.Fragment
@@ -8,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import androidx.navigation.fragment.findNavController
 import com.example.project.DBKeys
 import com.example.project.FavouritePayment
@@ -25,6 +27,7 @@ class PaymentFragment : Fragment() {
     private val user = DBKeys.user!!
     private var balance: Int? = null
     private var amount: Int? = null
+    private var bonus = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,14 +36,24 @@ class PaymentFragment : Fragment() {
         _binding = FragmentPaymentBinding.inflate(inflater, container, false)
         database = Firebase.database.getReference(DBKeys.USERS)
 
+        binding.amount.doOnTextChanged { text, _, _, _ ->
+            amount = text.toString().toIntOrNull()
+        }
+
         database.child(user).get().addOnSuccessListener {
             balance = it.child("card").child("balance").value.toString().toInt()
             binding.txtCardValue.text = balance.toString()
         }
 
+        bonusCheck()
+
         binding.btnPay.setOnClickListener {
-            amount = binding.amount.text.toString().toInt()
-            validateData()
+            if (!TextUtils.isEmpty(binding.amount.text)) {
+                amount = binding.amount.text.toString().toInt()
+                validateData()
+            } else {
+                binding.amount.error = "Please enter an amount of transfer"
+            }
         }
 
         return binding.root
@@ -48,10 +61,10 @@ class PaymentFragment : Fragment() {
 
     private fun validateData() {
         when {
-            amount!! == 0 || TextUtils.isEmpty(amount.toString()) -> {
+            amount!! == 0 || TextUtils.isEmpty(binding.amount.text) -> {
                 binding.amount.error = "Please enter an amount of transfer"
             }
-            balance!! <= amount!! -> Toast.makeText(
+            balance!! + bonus < amount!! -> Toast.makeText(
                 activity,
                 "You do not have so much money",
                 Toast.LENGTH_SHORT
@@ -65,17 +78,56 @@ class PaymentFragment : Fragment() {
 
     private fun setBalance() {
         database.child(user).child("card").child("balance")
-            .setValue(balance!! - amount!!).addOnSuccessListener {
+            .setValue(balance!! + bonus - amount!!).addOnSuccessListener {
                 Toast.makeText(activity, "Success", Toast.LENGTH_SHORT).show()
             }.addOnFailureListener {
                 Toast.makeText(activity, "Failed", Toast.LENGTH_SHORT).show()
             }
+        database.child(user).child("bonus").get().addOnSuccessListener {
+            database.child(user).child("bonus").setValue(it.value.toString().toInt() - bonus)
+        }
 
         binding.amount.onEditorAction(EditorInfo.IME_ACTION_DONE)
         binding.btnPay.onEditorAction(EditorInfo.IME_ACTION_DONE)
         switchCheck()
 
         findNavController().navigate(R.id.action_paymentFragment_to_paymentsFragment)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun bonusCheck() {
+        binding.switchSpendBonuses.setOnCheckedChangeListener { _, isChecked ->
+            if (!TextUtils.isEmpty(binding.amount.text) && binding.amount.text.toString() != "0") {
+                if (isChecked) {
+                    database.child(user).child("bonus").get().addOnSuccessListener {
+                        bonus = it.value.toString().toInt()
+                        val amountWithBonus: Int
+                        if (amount!! <= bonus) {
+                            amountWithBonus = 0
+                            bonus = amount!!
+                        } else {
+                            amountWithBonus = amount?.minus(bonus)!!
+                        }
+                        binding.txtSpentValue.text = "$amountWithBonus + $bonus"
+                        binding.txtSpent.visibility = View.VISIBLE
+                        binding.txtSpentValue.visibility = View.VISIBLE
+                    }
+                        .addOnFailureListener {
+                            bonus = 0
+                            binding.txtSpentValue.text = "$amount + $bonus"
+                            binding.txtSpent.visibility = View.VISIBLE
+                            binding.txtSpentValue.visibility = View.VISIBLE
+                        }
+
+                } else {
+                    binding.txtSpent.visibility = View.GONE
+                    binding.txtSpentValue.visibility = View.GONE
+                    bonus = 0
+                }
+            } else {
+                binding.amount.error = "Please enter an amount of transfer"
+            }
+        }
     }
 
     private fun switchCheck() {
--- app/src/main/java/com/example/project/fragments/PaymentToFavouriteFragment.kt
+++ app/src/main/java/com/example/project/fragments/PaymentToFavouriteFragment.kt
@@ -1,5 +1,6 @@
 package com.example.project.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.TextUtils
 import androidx.fragment.app.Fragment
@@ -8,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import androidx.navigation.fragment.findNavController
 import com.example.project.DBKeys
 import com.example.project.R
@@ -24,6 +26,7 @@ class PaymentToFavouriteFragment : Fragment() {
     private val user = DBKeys.user!!
     private var balance: Int? = null
     private var amount: Int? = null
+    private var bonus = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,6 +35,10 @@ class PaymentToFavouriteFragment : Fragment() {
         _binding = FragmentPaymentToFavouriteBinding.inflate(inflater, container, false)
         database = Firebase.database.getReference(DBKeys.USERS)
 
+        binding.amount.doOnTextChanged { text, _, _, _ ->
+            amount = text.toString().toIntOrNull()
+        }
+
         database.child(user).get().addOnSuccessListener {
             balance = it.child("card").child("balance").value.toString().toInt()
             binding.txtCardValue.text = balance.toString()
@@ -39,19 +46,25 @@ class PaymentToFavouriteFragment : Fragment() {
 
         binding.txtPayment.text = arguments?.getString("name")
 
+        bonusCheck()
+
         binding.btnPay.setOnClickListener {
-            amount = binding.amount.text.toString().toInt()
-            validateData()
+            if (!TextUtils.isEmpty(binding.amount.text)) {
+                amount = binding.amount.text.toString().toInt()
+                validateData()
+            } else {
+                binding.amount.error = "Please enter an amount of transfer"
+            }
         }
         return binding.root
     }
 
     private fun validateData() {
         when {
-            amount!! == 0 || TextUtils.isEmpty(amount.toString()) -> {
+            amount!! == 0 || TextUtils.isEmpty(binding.amount.text) -> {
                 binding.amount.error = "Please enter an amount of transfer"
             }
-            balance!! <= amount!! -> Toast.makeText(
+            balance!! + bonus < amount!! -> Toast.makeText(
                 activity,
                 "You do not have so much money",
                 Toast.LENGTH_SHORT
@@ -65,14 +78,53 @@ class PaymentToFavouriteFragment : Fragment() {
 
     private fun setBalance() {
         database.child(user).child("card").child("balance")
-            .setValue(balance!! - amount!!).addOnSuccessListener {
+            .setValue(balance!! + bonus - amount!!).addOnSuccessListener {
                 Toast.makeText(activity, "Success", Toast.LENGTH_SHORT).show()
             }.addOnFailureListener {
                 Toast.makeText(activity, "Failed", Toast.LENGTH_SHORT).show()
             }
+        database.child(user).child("bonus").get().addOnSuccessListener {
+            database.child(user).child("bonus").setValue(it.value.toString().toInt() - bonus)
+        }
 
         binding.amount.onEditorAction(EditorInfo.IME_ACTION_DONE)
         binding.btnPay.onEditorAction(EditorInfo.IME_ACTION_DONE)
         findNavController().navigate(R.id.action_paymentToFavouriteFragment_to_paymentsFragment)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun bonusCheck() {
+        binding.switchSpendBonuses.setOnCheckedChangeListener { _, isChecked ->
+            if (!TextUtils.isEmpty(binding.amount.text) && binding.amount.text.toString() != "0") {
+                if (isChecked) {
+                    database.child(user).child("bonus").get().addOnSuccessListener {
+                        bonus = it.value.toString().toInt()
+                        val amountWithBonus: Int
+                        if (amount!! <= bonus) {
+                            amountWithBonus = 0
+                            bonus = amount!!
+                        } else {
+                            amountWithBonus = amount?.minus(bonus)!!
+                        }
+                        binding.txtSpentValue.text = "$amountWithBonus + $bonus"
+                        binding.txtSpent.visibility = View.VISIBLE
+                        binding.txtSpentValue.visibility = View.VISIBLE
+                    }
+                        .addOnFailureListener {
+                            bonus = 0
+                            binding.txtSpentValue.text = "$amount + $bonus"
+                            binding.txtSpent.visibility = View.VISIBLE
+                            binding.txtSpentValue.visibility = View.VISIBLE
+                        }
+
+                } else {
+                    binding.txtSpent.visibility = View.GONE
+                    binding.txtSpentValue.visibility = View.GONE
+                    bonus = 0
+                }
+            } else {
+                binding.amount.error = "Please enter an amount of transfer"
+            }
+        }
     }
 }
